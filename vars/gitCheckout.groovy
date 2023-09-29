20adc7a4fff976aad12a6a74a862a1af15d912e7
Gurubasavaraj <135728635+samplegithub2@users.noreply.github.com>
2023-09-29 12:56:24 +0530

    all added

---
+++ vars/gitCheckout.groovy
@@ -0,0 +1,8 @@
+def call(String git_branch = 'main', String git_credentials = null, String git_url = null) {    
+        echo "This is gitCheckout.groovy"
+        checkout([$class: 'GitSCM', 
+                  branches: [[name: "*/${git_branch}"]], 
+                  userRemoteConfigs: [[credentialsId: "${git_credentials}", url: "${git_url}"]]
+                ])  
+        sh 'pwd; ls -lrt'                   
+}
